4556d1e2df272bf2fc3b016c4ffe52e2c07b4dc5
Charsov Denis <charsovden@gmail.com>
2026-06-29 01:49:23 +0300

    write method tests to get from timetable

--- test/ru/yandex/practicum/gym/TimetableTest.java
+++ test/ru/yandex/practicum/gym/TimetableTest.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.gym;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -18,8 +18,14 @@ public class TimetableTest {
 
         timetable.addNewTrainingSession(singleTrainingSession);
 
+        TreeMap<TimeOfDay, HashSet<TrainingSession>> sessionsForMonday = timetable.getTrainingSessionsForDay(DayOfWeek.MONDAY);
         //Проверить, что за понедельник вернулось одно занятие
+        assertNotNull(sessionsForMonday);
+        assertEquals(1, sessionsForMonday.get(new TimeOfDay(13, 0)).size());
+
         //Проверить, что за вторник не вернулось занятий
+        TreeMap<TimeOfDay, HashSet<TrainingSession>> sessionsForTuesday = timetable.getTrainingSessionsForDay(DayOfWeek.TUESDAY);
+        assertNull(sessionsForTuesday);
     }
 
     @Test
@@ -47,8 +53,29 @@ public class TimetableTest {
         timetable.addNewTrainingSession(saturdayChildTrainingSession);
 
         // Проверить, что за понедельник вернулось одно занятие
+        TreeMap<TimeOfDay, HashSet<TrainingSession>> sessionsForMonday = timetable.getTrainingSessionsForDay(DayOfWeek.MONDAY);
+        assertNotNull(sessionsForMonday.get(new TimeOfDay(13, 0)));
+        assertEquals(1, sessionsForMonday.get(new TimeOfDay(13, 0)).size());
         // Проверить, что за четверг вернулось два занятия в правильном порядке: сначала в 13:00, потом в 20:00
+        TreeMap<TimeOfDay, HashSet<TrainingSession>> sessionsForThursday = timetable.getTrainingSessionsForDay(DayOfWeek.THURSDAY);
+        assertNotNull(sessionsForThursday);
+        assertEquals(2, sessionsForThursday.size());
+
+        var iteratorByEntry = sessionsForThursday.entrySet().iterator();
+        Map.Entry<TimeOfDay, HashSet<TrainingSession>> firstEntry = iteratorByEntry.next();
+
+        assertEquals(new TimeOfDay(13, 0), firstEntry.getKey());
+        assertEquals(1, firstEntry.getValue().size());
+        assertTrue(firstEntry.getValue().contains(thursdayChildTrainingSession));
+
+        Map.Entry<TimeOfDay, HashSet<TrainingSession>> secondEntry = iteratorByEntry.next();
+        assertEquals(new TimeOfDay(20, 0), secondEntry.getKey());
+        assertEquals(1, secondEntry.getValue().size());
+        assertTrue(secondEntry.getValue().contains(thursdayAdultTrainingSession));
+
         // Проверить, что за вторник не вернулось занятий
+        TreeMap<TimeOfDay, HashSet<TrainingSession>> sessionsForTuesday = timetable.getTrainingSessionsForDay(DayOfWeek.TUESDAY);
+        assertNull(sessionsForTuesday);
     }
 
     @Test
@@ -63,7 +90,14 @@ public class TimetableTest {
         timetable.addNewTrainingSession(singleTrainingSession);
 
         //Проверить, что за понедельник в 13:00 вернулось одно занятие
+        HashSet<TrainingSession> sessionsAt13Monday = timetable.getTrainingSessionsForDayAndTime(
+                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+        assertNotNull(sessionsAt13Monday);
+        assertEquals(1, sessionsAt13Monday.size());
+        assertTrue(sessionsAt13Monday.contains(singleTrainingSession));
         //Проверить, что за понедельник в 14:00 не вернулось занятий
+        HashSet<TrainingSession> sessionsAt14Monday = timetable.getTrainingSessionsForDayAndTime(
+                DayOfWeek.MONDAY, new TimeOfDay(14, 0));
+        assertNull(sessionsAt14Monday);
     }
-
 }
