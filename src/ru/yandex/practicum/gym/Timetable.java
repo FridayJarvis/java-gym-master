881ce3d4a6593b4c2b6188e1d5ea26fddf90fa9e
Charsov Denis <charsovden@gmail.com>
2026-06-28 22:52:29 +0300

    write add(), get() methods and choose architecture of trainings storage

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -1,20 +1,42 @@
 package ru.yandex.practicum.gym;
 
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private HashMap<DayOfWeek, TreeMap<TimeOfDay, HashSet<TrainingSession>>> timetable;
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
         //сохраняем занятие в расписании
+        TreeMap<TimeOfDay, HashSet<TrainingSession>> sessionsForDay = timetable.get(trainingSession.getDayOfWeek());
+        if (sessionsForDay == null) {
+            sessionsForDay = new TreeMap<>();
+            timetable.put(trainingSession.getDayOfWeek(), sessionsForDay);
+        }
+
+        HashSet<TrainingSession> sessionsForTime = sessionsForDay.get(trainingSession.getTimeOfDay());
+        if (sessionsForTime == null) {
+            sessionsForTime = new HashSet<>();
+            sessionsForDay.put(trainingSession.getTimeOfDay(), sessionsForTime);
+        }
+
+        sessionsForTime.add(trainingSession);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+    public TreeMap<TimeOfDay, HashSet<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
+        return timetable.get(dayOfWeek);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+    public HashSet<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
+        TreeMap<TimeOfDay, HashSet<TrainingSession>> sessionsForDay = timetable.get(dayOfWeek);
+        if (sessionsForDay == null) {
+            return null;
+        }
+
+        return sessionsForDay.get(timeOfDay);
     }
 }
